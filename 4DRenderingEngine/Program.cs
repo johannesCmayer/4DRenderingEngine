b427368411fce16d080137ecf4138a23b3d67cd4
Spyion <m4tm4t380@gmail.com>
2018-09-08 13:50:17 +0200

    init

---
+++ 4DRenderingEngine/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenGL;
+
+
+namespace _4DRenderingEngine
+{
+    class Program : GameWindow 
+    {
+        public Program program;
+        public Program()
+        {
+            program = this;
+        }
+
+        static void Main(string[] args) => new Program().Run();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Dispose();
+        }
+    }
+}
